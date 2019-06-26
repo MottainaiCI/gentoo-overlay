b28a6a5135bbfc76e4a432a18637d1a840ea8f92
Ettore Di Giacinto <mudler@gentoo.org>
2019-06-26 13:06:55 +0200

    scripts: Add target branch

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 set -v
+
+BRANCH="${BRANCH:-develop}"
 #SERVER_SHA=$(curl https://api.github.com/repos/MottainaiCI/mottainai-server/commits/master |  jq -r '.  | .sha')
 #AGENT_SHA=$(curl https://api.github.com/repos/MottainaiCI/mottainai-agent/commits/master |  jq -r '.  | .sha')
 #CLI_SHA=$(curl https://api.github.com/repos/MottainaiCI/mottainai-cli/commits/master |  jq -r '.  | .sha')
@@ -11,12 +13,12 @@ DATE=$(date +'%Y%m%d')
 for repo in "mottainai-server" "mottainai-cli" "mottainai-agent" "replicant"
 do
 
-	SHA=$(curl "https://api.github.com/repos/MottainaiCI/$repo/commits/master" |  jq -r '.  | .sha')
+	SHA=$(curl "https://api.github.com/repos/MottainaiCI/$repo/commits/$BRANCH" |  jq -r '.  | .sha')
 	cp -rfv "dev-util/$repo/$repo-9999.ebuild" "dev-util/${repo}/${repo}-0.0_pre${DATE}.ebuild"
 	CURRENT_SHA=$(grep -Po 'EGIT_COMMIT="\K.*(?=")' "dev-util/${repo}/${repo}-0.0_pre${DATE}.ebuild")
 	sed -i "s:${CURRENT_SHA}:${SHA}:g" "dev-util/${repo}/${repo}-0.0_pre${DATE}.ebuild"
 	pushd "dev-util/${repo}"
 		git add "${repo}-0.0_pre${DATE}.ebuild"
-		repoman commit -m "dev-util/$repo: Pin to $DATE"
+		repoman commit -m "dev-util/$repo: Pin to $BRANCH-$DATE"
 	popd
 done
